5d54ec98b212096851c2dbfc94eb0f4231a2cafa
mufaqar <mufaqar@gmail.com>
2023-08-27 14:32:03 +0500

    curel

--- super/super-repair.php
+++ super/super-repair.php
@@ -1,82 +1,82 @@
-    <?php /* Template Name: Super-Repair_List  */    get_header('admin');
+    <?php /* Template Name: Reports  */    get_header('admin');
+
+
+$curl = curl_init();
+
+
+
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://sav.jcen.cn/pwsys/sys/gettoken',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+"appid":"shabbir",
+"appsecret":"shabbir-123",
+"code":"892894b98ceb5f11660d6cd3fff5c3d1"
+}
+
+',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+
+exit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://sav.jcen.cn/pwsys/pwtransiot/pwTransIot/list_xzairport',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_SSL_VERIFYPEER => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+    "meterNum": "230729010001",
+    "pageSize": 1000,
+    "pageNo": 1000
+}
+',
+  CURLOPT_HTTPHEADER => array(
+    'X-Access-Token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTMxMjk4MDksInVzZXJuYW1lIjoic2hhYmJpciJ9.tyWyts9IYxfGgRgeeQ-C92X-HZE-Y_ObgLXQ-V3PykA',
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+echo "123456"
 
 
     ?>
-    <style>
 
-#div1 { width: 90% !important;margin: 0px auto;}
-
-</style>
-        <?php include('navigation.php'); ?>
-        <div class="admin_parrent">
-            
-            <section id="div1" class="targetDiv activediv tablediv">
-            <table id="invoice_orders" class="table table-striped orders_table export_table" style="width:100%">
-            <thead>
-                        <tr>
-                            <th>Sr #</th>
-                            <th>Model</th>
-                            <th>Fault Type</th>                         
-                            <th>Type</th>                             
-                            <th>Parts availbiility</th>
-                           
-                         
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    <?php 
-                        $i = 0;
-                        query_posts(array(
-                            'post_type' => 'repair',
-                            'posts_per_page' => -1,
-                            'order' => 'desc'
-                        ));
-
-                        if (have_posts()) :  while (have_posts()) : the_post();$pid = get_the_ID();
-                                $i++;
-                                $diagnostic_fee = get_post_meta(get_the_ID(), 'diagnostic_fee', true); 
-                                $parts_availablity = get_post_meta($pid, 'parts_availablity', true);                                 
-                                $repair_cost = get_post_meta( $pid, 'repair_cost', true );                             
-                                $cat_status  = get_the_terms( $pid, 'cat_fault_type');
-                                foreach($cat_status as $cat_selected)
-                                {
-                             
-                                  $cat_name =  $cat_selected->name;
-                                }
-                                
-                                $repair_cats  = get_the_terms( $pid, 'repair_cat');
-                                foreach($repair_cats as $repair_cat)
-                                {
-                          
-                                  $repair_cat_name =  $repair_cat->name;
-                                }
-                                ?>
-                                <tr>
-                                    <td><?php echo $i ?></td>
-                                    <td><?php the_title() ?></td> 
-                                    <td><?php echo $cat_name;?></td>
-                                    <td><?php echo $repair_cat_name;?></td>
-                                    <td><?php echo $parts_availablity;?></td>                                                            
-                                   
-                                   
-                                
-                                </tr>
-                            <?php endwhile;
-                            wp_reset_query();
-                        else : ?>
-                            <h2><?php _e('Nothing Found', 'lbt_translate'); ?></h2>
-                        <?php endif; ?>
-
-                    </tbody>
-
-                </table>
-
-            </section>
-
-        </div>
-
-
-
-   
 <?php get_footer('admin') ?>
