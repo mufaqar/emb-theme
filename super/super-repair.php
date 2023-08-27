2dfe8919db11c27f15d3f444fbb579a3d3eec308
mufaqar <mufaqar@gmail.com>
2023-08-27 14:46:56 +0500

    response

--- super/super-repair.php
+++ super/super-repair.php
@@ -1,39 +1,65 @@
     <?php /* Template Name: Reports  */    get_header();
 
 
-$curl = curl_init();
+
+// $url = 'https://sav.jcen.cn/pwsys/sys/gettoken';
+// $args = array(
+//     'body' => json_encode(array(
+//         'appid' => 'shabbir',
+//         'appsecret' => 'shabbir-123',
+//         'code' => '892894b98ceb5f11660d6cd3fff5c3d1',
+//     )),
+//     'headers' => array(
+//         'Content-Type' => 'application/json',
+//     ),
+//     'method' => 'POST',
+//     'sslverify' => true, // Set to true to enable SSL verification
+// );
+
+// $response = wp_remote_request($url, $args);
+
+// if (is_array($response) && !is_wp_error($response)) {
+//     $body = wp_remote_retrieve_body($response);
+//     // Process $body as needed
+// }
 
 
+$url = 'https://sav.jcen.cn/pwsys/pwtransiot/pwTransIot/list_xzairport';
 
+$args = array(
+    'body' => json_encode(array(
+        'meterNum' => '230729010001',
+        'pageSize' => 1000,
+        'pageNo' => 1,
+    )),
+    'headers' => array(
+        'X-Access-Token' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTMxMzExODIsInVzZXJuYW1lIjoic2hhYmJpciJ9.EaHxm_aKHCD-TNgat50iXSnFznIdIW9hAsqjdNoPvW0',
+        'Content-Type' => 'application/json',
+    ),
+    'method' => 'POST',
+);
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://sav.jcen.cn/pwsys/sys/gettoken',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{
-"appid":"shabbir",
-"appsecret":"shabbir-123",
-"code":"892894b98ceb5f11660d6cd3fff5c3d1"
+$response = wp_remote_post($url, $args);
+
+print_r($response);
+
+if (is_array($response) && !is_wp_error($response)) {
+    $body = wp_remote_retrieve_body($response);
+    echo $body;
 }
 
-',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
 
 
-echo "Working fine";
+
+
+
+
+
+
+
+
+
+
 
 
     ?>
