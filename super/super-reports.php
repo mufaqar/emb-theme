c17225e0b71321c157c13e1a9eb3994a0730be67
mufaqar <mufaqar@gmail.com>
2023-08-28 15:42:32 +0500

    reports

--- super/super-reports.php
+++ super/super-reports.php
@@ -4,7 +4,7 @@
 ?>
     <?php include('navigation.php'); ?>
     <div class="admin_parrent">
-        <div class="toggle_btn">
+        <!-- <div class="toggle_btn">
             <div class="row ">
                 <div class="catering_wrapper mt-5 mb-2  p-0 w-100">
                     <div class="catering_menu buttons">
@@ -16,21 +16,21 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <section id="div1" class="targetDiv activediv tablediv">
         <table id="invoice_orders" class="table table-striped orders_table export_table" style="width:100%">
         <thead>
                     <tr>
                         <th>Sr #</th>
-                        <th>Invoice Id</th>   
-                        <th>Ticket</th>
-                        <th>Model</th>
-                        <th>Agent</th>                         
-                        <th>Order Date</th>                             
-                        <th>Price</th>
-                        <th>Amout Paid</th>
-                        <th>Status</th>
-                        <th>Action</th>
+                        <th>ID</th>   
+                        <th>Station Id</th>
+                        <th>Transformer Id</th>
+                        <th>Dev Id</th>                         
+                        <th>Dev Name</th>                             
+                        <th>Oper date</th>
+                        <th>QTY</th>
+                        <th>Vol A</th>
+                     
                     </tr>
                 </thead>
                 <tbody>
@@ -38,35 +38,40 @@
                 <?php 
                     $i = 0;
                     query_posts(array(
-                        'post_type' => 'orders',
+                        'post_type' => 'records',
                         'posts_per_page' => -1,
                         'order' => 'desc'
                     ));
 
                     if (have_posts()) :  while (have_posts()) : the_post();$pid = get_the_ID();
                             $i++;
-                            $ticket_id = get_post_meta(get_the_ID(), 'order_id', true); 
-                            $ticket_agent = get_post_meta($ticket_id, 'order_uid', true); 
-                            $user_id = get_post_meta( $ticket_id, 'order_uid', true );
-                            $user_info = get_userdata($user_id);  
-                            $cat_status  = get_the_terms( $pid, 'order_status');
-                            foreach($cat_status as $cat_selected)
-                            {
-                              $cat_active =  $cat_selected->slug;
-                              $cat_name =  $cat_selected->name;
-                            }  
+                          
+
+                            $id = get_post_meta(get_the_ID(),'id', true); 
+                            $stationid = get_post_meta(get_the_ID(),'stationid', true); 
+                            $transformerid = get_post_meta(get_the_ID(),'transformerid', true); 
+                            $transformername = get_post_meta(get_the_ID(),'transformername', true); 
+                            $devid = get_post_meta(get_the_ID(),'devid', true); 
+                            $devname = get_post_meta(get_the_ID(),'devname', true); 
+                            $devnum  = get_post_meta(get_the_ID(),'devnum', true); 
+                            $operdate = get_post_meta(get_the_ID(),'operdate', true); 
+                            $qty_total = get_post_meta(get_the_ID(),'qty_total', true); 
+                            $vol_a  = get_post_meta(get_the_ID(),'vol_a', true); 
+                            $relay1state = get_post_meta(get_the_ID(),'relay1state', true);    
+                            
                             ?>
                             <tr>
                                 <td><?php echo $i ?></td>
-                                <td><?php the_title() ?></td> 
-                                <td><?php echo $ticket_id;?></td>
-                                <td><?php echo get_the_title($ticket_id);?></td>
-                                <td><?php  echo  $user_info->user_login ?></td>                                  
-                                <td><?php  echo  get_the_modified_date(); ?></td>                          
-                                <td>$ <?php echo get_post_meta(get_the_ID(), 'order_price', true); ?></td>
-                                <td>$ <?php echo get_post_meta(get_the_ID(), 'order_price_paid', true); ?></td>
-                                <td><span class="status <?php  echo $cat_active ?>"><?php echo $cat_name ?> </span> </td>
-                                <td> <a href="<?php echo home_url('admin-dashboard/edit-invoice?id='.$pid.''); ?>">Edit </a>  <i class="fa-solid fa-down-to-line"></i></td>
+                                <td><?php  echo $id?></td> 
+                                <td><?php echo $stationid;?></td>
+                                <td><?php echo $transformerid?></td>
+                                <td><?php  echo $devid ?></td>                                  
+                                <td><?php  echo $devname ?></td>   
+                                <td><?php  echo $operdate ?></td>   
+                                <td><?php  echo $qty_total ?></td>   
+                                <td><?php  echo $vol_a ?></td>   
+                                                     
+                             
                             </tr>
                         <?php endwhile;
                         wp_reset_query();
