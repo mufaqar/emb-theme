5371ff6c05df0e237224f48808b54499e5d8f613
mufaqar <mufaqar@gmail.com>
2023-08-31 16:31:49 +0500

    Status

--- super/super-api_reports.php
+++ super/super-api_reports.php
@@ -2,18 +2,13 @@
 $token =  Is_Token_Expired(); 
 
 //echo $token;
-
-
 $url = 'https://sav.jcen.cn/pwsys/pwtransiot/pwTransIot/list_xzairport';
-
 $args = array(
     'body' => json_encode(array(
         'startTime' => '2023-08-20',       
         'endTime' => "2023-08-30",
         'pageNo' => 1,
         'pageSize' => 1000,
-
-
     )),
     'headers' => array(
         'X-Access-Token' => $token,
@@ -24,11 +19,6 @@ $args = array(
 );
 
 $response = wp_remote_post($url, $args);
-
-
-
-
-
 ?>
     <?php include('navigation.php'); ?>
     <div class="admin_parrent">        
@@ -37,17 +27,12 @@ $response = wp_remote_post($url, $args);
         <thead>
                     <tr>
                         <th>Sr #</th>
-                 
-                        <th>Station Id</th>
-                        <th>Transformer Id</th>
                         <th>Dev Id</th>                         
                         <th>Dev Name</th>                             
                         <th>Oper date</th>
                         <th>QTY</th>
                         <th>Vol A</th>
-                        <th>relay1state</th>
-
-                        
+                        <th>Status </th> 
                      
                     </tr>
                 </thead>
@@ -62,7 +47,7 @@ if (is_array($response) && !is_wp_error($response)) {
         
 
           
-                $i = 0;
+                            $i = 0;
                    
                             $i++;
                             $id = $record['id'];
@@ -76,19 +61,10 @@ if (is_array($response) && !is_wp_error($response)) {
                             $qty_total = $record['qty_total'];
                             $vol_a  = $record['vol_a'];
                             $relay1state = $record['relay1state'];
-
-            
-
-
-                    
-
-                          
                             
                             ?>
                             <tr>
-                                <td><?php echo $i ?></td>                            
-                                <td><?php echo $stationid;?></td>
-                                <td><?php echo $transformerid?></td>
+                                <td><?php echo $i ?></td>   
                                 <td><?php  echo $devid ?></td>                                  
                                 <td><?php  echo $devname ?></td>   
                                 <td><?php  echo $operdate ?></td>   
