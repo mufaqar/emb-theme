d709c051368f4d2759599b12ca95131319d3fdbf
mufaqar <mufaqar@gmail.com>
2023-08-31 18:18:52 +0500

    update

--- super/super-remote.php
+++ super/super-remote.php
@@ -39,9 +39,7 @@ get_header('admin');
                     $terminal_branch_name = get_post_meta($pid , 'terminal_branch_name', true);
                     $terminal_floor_section = get_post_meta($pid , 'terminal_floor_section', true);
 
-                    $user_info = get_userdata($terminal_company);
-
-                  
+                    $user_info = get_userdata($terminal_company);                  
                     $post_status = get_post_status($pid);                 
                         if ($post_status === 'publish') {
                             $text_status = 'Active';
@@ -58,10 +56,13 @@ get_header('admin');
                     <th><?php echo $terminal_branch_name?></th>
                     <th><?php echo $terminal_floor_section ?></th>
                     <th><?php echo $text_status ?></th>
-                    <th><div class="custom-control custom-switch">
-  <input type="checkbox" class="custom-control-input" id="onOffSwitch">
-  <label class="custom-control-label" for="onOffSwitch">On/Off</label>
-</div></th>
+                    <th>
+                    <form name="change_status">
+                        <label><input type="radio" value="On" name="statuschange">On</label>
+                        <label><input type="radio" value="Off" name="statuschange">Off</label>
+                    </form>
+                    
+                        </div></th>
 
 
 
@@ -85,3 +86,31 @@ get_header('admin');
 
 
 <?php get_footer('admin') ?>
+
+<script>
+        // Get a reference to the radio buttons
+        const radioButtons = document.getElementsByName('statuschange');     
+        radioButtons.forEach(radioButton => {
+            radioButton.addEventListener('change', function() {
+                // Check which option is selected
+                if (this.value === 'On') {
+                    // Call a function when "On" is selected
+                    handleOn();
+                } else if (this.value === 'Off') {
+                    // Call a function when "Off" is selected
+                    handleOff();
+                }
+            });
+        });
+
+        // Define the functions to call when the options are selected
+        function handleOn() {
+            console.log('Status turned On');
+            // You can perform other actions here
+        }
+
+        function handleOff() {
+            console.log('Status turned Off');
+            // You can perform other actions here
+        }
+    </script>
