9715c2f830f8d07a67df848b6cf4d59a1b90fcff
mufaqar <mufaqar@gmail.com>
2023-09-11 19:28:51 +0500

    update

--- super/super-importdata.php
+++ super/super-importdata.php
@@ -41,70 +41,64 @@
         // Access the "records" array
         $records = $data->result->records;
 
-
-
-    foreach ($records as $array_data) {           
-
-            $id = $array_data['id'];
-            $stationid = $array_data['stationid'];
-            $transformerid = $array_data['transformerid'];
-            $transformername = $array_data['transformername'];
-            $devid = $array_data['devid'];
-            $devname = $array_data['devname'];
-            $devnum  = $array_data['devnum'];
-            $operdate = $array_data['operdate'];
-            $qty_total = $array_data['qty_total'];
-            $vol_a  = $array_data['vol_a'];
-            $relay1state = $array_data['relay1state'];   
-            $power_a = $array_data['power_a']; 
-            $amp_a = $array_data['amp_a'];               
-            $post_title = $stationid."-".$id;          
-            $post_data = array(
-                'post_title' => $post_title,
-                'post_content' => $stationid,
-                'post_type' => 'records', // Change to your desired post type
-                'post_status' => 'publish',
-                'meta_input' => array(
-                    'id' => $array_data['id'],
-                    'stationid' => $array_data['stationid'],
-                    'transformerid' => $array_data['transformerid'],
-                    'transformername' => $array_data['transformername'],
-                    'devid' => $array_data['devid'],
-                    'devname' => $array_data['devname'],
-                    'devnum' => $array_data['devnum'],
-                    'operdate' => $array_data['operdate'],
-                    'qty_total' => $array_data['qty_total'],
-                    'vol_a' => $array_data['vol_a'],
-                    'relay1state' => $array_data['relay1state'],
-                    'power_a' => $array_data['power_a'],
-                    'amp_a' => $array_data['amp_a'],
-
-                )
-            );
-
-            // Insert the post with metadata
-            $post_id = wp_insert_post($post_data);
-            echo "Post inserted with ID: " . $post_id . "<br>";
-            if (is_wp_error($post_id)) {
-                echo "Error inserting post: " . $post_id->get_error_message();
-            } else {
-                echo "Post inserted with ID: " . $post_id . "<br>";
-            } 
-
-    
-    
+        if (is_wp_error($response)) {
+            // Handle the error, if any
+        } else {
+            $body = wp_remote_retrieve_body($response);
+            $data = json_decode($body);
+        
+            // Access the "records" array
+            $records = $data->result->records;
+        
+            foreach ($records as $array_data) {
+                $id = $array_data->id;
+                $stationid = $array_data->stationid;
+                $transformerid = $array_data->transformerid;
+                $transformername = $array_data->transformername;
+                $devid = $array_data->devid;
+                $devname = $array_data->devname;
+                $devnum  = $array_data->devnum;
+                $operdate = $array_data->operdate;
+                $qty_total = $array_data->qty_total;
+                $vol_a  = $array_data->vol_a;
+                $relay1state = $array_data->relay1state;
+                $power_a = $array_data->power_a;
+                $amp_a = $array_data->amp_a;
+                $post_title = $stationid . "-" . $id;
+                $post_data = array(
+                    'post_title' => $post_title,
+                    'post_content' => $stationid,
+                    'post_type' => 'records', // Change to your desired post type
+                    'post_status' => 'publish',
+                    'meta_input' => array(
+                        'id' => $id,
+                        'stationid' => $stationid,
+                        'transformerid' => $transformerid,
+                        'transformername' => $transformername,
+                        'devid' => $devid,
+                        'devname' => $devname,
+                        'devnum' => $devnum,
+                        'operdate' => $operdate,
+                        'qty_total' => $qty_total,
+                        'vol_a' => $vol_a,
+                        'relay1state' => $relay1state,
+                        'power_a' => $power_a,
+                        'amp_a' => $amp_a,
+                    )
+                );
+        
+                // Insert the post with metadata
+                $post_id = wp_insert_post($post_data);
+                if (is_wp_error($post_id)) {
+                    echo "Error inserting post: " . $post_id->get_error_message();
+                } else {
+                    echo "Post inserted with ID: " . $post_id . "<br>";
+                }
+            }
         }
+        
 
-      
-
-     }
-    
-
-  
-
-  
-
-
+    }
 
 
 
